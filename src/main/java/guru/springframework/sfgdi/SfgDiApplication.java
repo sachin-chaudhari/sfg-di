cfb50ea39b34d596fa6e7929fcabe4e68dada0f9
sachincha <sachincha@sachincha-win8.cybage.com>
2020-02-10 16:14:07 +0530

    committing

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -2,12 +2,21 @@ package guru.springframework.sfgdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.sfgdi.controllers.MyController;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+		
+		MyController controller = (MyController) ctx.getBean("myController");
+		
+		String greeting = controller.sayHello();
+		
+		System.out.println(greeting);
 	}
 
 }
